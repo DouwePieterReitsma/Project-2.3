9e6dc01b0390570cce5d81ee4cb90490364c4f57
DouwePieterReitsma <d.p.reitsma@gmail.com>
2019-03-26 23:24:16 +0100

    build.gradle gefixt

--- src/main/java/gameai/Main.java
+++ src/main/java/gameai/Main.java
@@ -1,7 +1,25 @@
 package gameai;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("views/main.fxml"));
+
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("GameAI");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch();
     }
 }
---
+++ src/main/java/gameai/controllers/MainController.java
@@ -0,0 +1,4 @@
+package gameai.controllers;
+
+public class MainController {
+}
