663607cad4ad9ad4d2c04504c4d634b39f53fbb8
DouwePieterReitsma <d.p.reitsma@gmail.com>
2019-04-09 11:21:14 +0200

    code snippet in main geplaatst

--- src/main/java/gameai/Main.java
+++ src/main/java/gameai/Main.java
@@ -4,6 +4,12 @@ import gameai.models.Board;
 import gameai.models.MiniMaxTicTacToe;
 import gameai.models.RandomTicTacToeAI;
 import gameai.models.TicTacToeBoard;
+import gameai.models.othello.OthelloBoard;
+import gameai.models.othello.OthelloColor;
+import gameai.models.othello.OthelloPiece;
+import gameai.models.othello.ai.AlphaBetaOthelloAI;
+import gameai.models.othello.ai.OthelloAI;
+import gameai.models.othello.ai.RandomOthelloAI;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -26,5 +32,27 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch();
         Board board = new TicTacToeBoard(new MiniMaxTicTacToe());
+
+//        OthelloBoard board = new OthelloBoard(OthelloColor.WHITE);
+//
+//        OthelloPiece wp = new OthelloPiece(OthelloColor.WHITE);
+//        OthelloPiece bp = new OthelloPiece(OthelloColor.BLACK);
+//
+//        board.getPositions()[3][3].setPiece(bp);
+//        board.getPositions()[4][3].setPiece(wp);
+//        board.getPositions()[3][4].setPiece(wp);
+//        board.getPositions()[4][4].setPiece(bp);
+//
+//        System.out.println(board);
+//
+//        OthelloAI white = new AlphaBetaOthelloAI(board, OthelloColor.WHITE, 5);
+//        OthelloAI black = new RandomOthelloAI(board, OthelloColor.BLACK);
+//
+//        while(!board.isGameOver()) {
+//            white.play();
+//            black.play();
+//        }
+//
+//        System.out.println(board.getMatchResult());
     }
 }
