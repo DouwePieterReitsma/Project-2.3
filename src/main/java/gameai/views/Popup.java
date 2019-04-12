6a21cb46380d4c0f3d50a644504ad6143c1a4532
Bram18 <34652399+Bram18@users.noreply.github.com>
2019-04-12 11:57:01 +0200

    popup

--- src/main/java/gameai/views/Popup.java
+++ src/main/java/gameai/views/Popup.java
@@ -3,7 +3,9 @@ package gameai.views;
 import java.io.IOException;
 
 import gameai.controllers.ConnectionListenerThread;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -11,17 +13,17 @@ import javafx.stage.*;
 
 
 public class Popup {
-	private static String uitdager;
-	private static String spel;
-	private static String nummer;
-	private static Stage popupwindow;
-	private static ConnectionListenerThread connectThreadd;
+	private String player;
+	private String game;
+	private String id;
+	private Stage popupwindow;
+	private ConnectionListenerThread connectThread;
 
-	public static void display(String player, String id , String game, ConnectionListenerThread connectThread) {
-		uitdager = player;
-		spel = game;
-		nummer = id;
-	    connectThreadd = connectThread;
+	public void display(String player, String id , String game, ConnectionListenerThread connectThread) {
+		this.player = player;
+		this.game = game;
+		this.id = id;
+	    this.connectThread = connectThread;
 
 		popupwindow=new Stage();
 
@@ -29,7 +31,7 @@ public class Popup {
 		popupwindow.setTitle("Challenge");
 
 
-		Label label1= new Label(uitdager +" daagt je uit om " + spel + " te spelen");
+		Label label1= new Label(player +" daagt je uit om " + game + " te spelen");
 
 		Button Accept= new Button("Accepteren ");
 		Button Decline= new Button("Weigeren");
@@ -47,18 +49,22 @@ public class Popup {
 		GridPane layout = new GridPane();
 		layout.add(label1, 1, 1);
 		layout.add(Accept, 2, 2);
-		layout.add(Decline, 1, 3);
+		layout.add(Decline, 3, 2);
+		layout.setConstraints(label1, 0, 0, 1, 1, HPos.RIGHT, VPos.CENTER, Priority.ALWAYS, Priority.ALWAYS);
+		layout.setConstraints(Accept, 0, 1, 1, 1, HPos.LEFT, VPos.CENTER, Priority.ALWAYS, Priority.ALWAYS);
+		layout.setConstraints(Decline, 1, 1, 1, 1, HPos.RIGHT, VPos.CENTER, Priority.ALWAYS, Priority.ALWAYS);
+		
 
-		Scene scene1= new Scene(layout, 300, 250);
+		Scene scene1= new Scene(layout, 400, 150);
 
 		popupwindow.setScene(scene1);
 
 		popupwindow.showAndWait();
 	}
 
-	private static void accept() throws IOException {
-		String send = "challenge accept " + nummer + "/n" ;
-		connectThreadd.sendCommand(send);
+	private void accept() throws IOException {
+		String send = "challenge accept " + id + "/n" ;
+		connectThread.sendCommand(send);
 		popupwindow.close();
 	}
 
