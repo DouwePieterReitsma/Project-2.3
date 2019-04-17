2e8119a77501b4e7dea1e91d59c4fc2c219693d3
DouwePieterReitsma <d.p.reitsma@gmail.com>
2019-04-17 13:55:19 +0200

    javadocs

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"gameai"},{"l":"gameai.controllers"},{"l":"gameai.models"},{"l":"gameai.models.othello"},{"l":"gameai.models.othello.ai"},{"l":"gameai.views"}]
